a8271c998868e3c5e3b6926395935c54df762400
asketsystem <asketsystem1@gmail.com>
2022-04-10 03:10:25 +0000

    Router

---
+++ src/presentation/routers/contact-router.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import { CreateContactUseCase } from '../../domain/interfaces/use-cases/contact/create-contact';
+import { GetAllContactsUseCase } from '../../domain/interfaces/use-cases/contact/get-all-contacts';
+
+
+export default function ContactRouter(
+    getAllContactsUseCase: GetAllContactsUseCase,
+    createContactUseCase: CreateContactUseCase
+) {
+    const router = express.Router();
+
+    router.get('/', async (req: Request, res: Response) => {
+        try {
+            const contacts = await getAllContactsUseCase.execute();
+            res.send(contacts);
+        } catch (err) {
+            res.status(500).send({ message: "Error fetching data" });
+        }
+    });
+
+    router.post('/', async (req: Request, res: Response) => {
+        try {
+            await createContactUseCase.execute(req.body);
+            res.statusCode = 201;
+            res.json({ message: "Contact created" });
+        } catch (err) {
+            res.status(500).send({ message: "Error creating contact" });
+        }
+    });
+
+    return router;
+}
